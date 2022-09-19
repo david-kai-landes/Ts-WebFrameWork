f9129de71cd23f19463ca69987b4f880b8ec7ad1
David Landes <davidkailandes@gmail.com>
2022-09-19 15:47:45 -0400

    added index.ts and User.ts, some logic added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log(" YEEHAAAAAA !!!!");
+import { User } from "./modals/User";
+
+const user = new User({ name: "my name", age: 20 });
+
+user.set({ name: "diffName" });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
