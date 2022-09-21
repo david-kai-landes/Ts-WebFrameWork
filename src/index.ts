60858910891ae95002ca90d92790b5b62c870614
David Landes <davidkailandes@gmail.com>
2022-09-21 10:35:04 -0400

    implemented the ability to add new users to db.json

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 import { User } from "./modals/User";
 
-const user = new User({ name: "my name", age: 20 });
+const user = new User({ name: " New Record ", age: 0 });
 
-user.on("change", () => {
-  console.log("change #1");
-});
-user.on("change", () => {
-  console.log("change #2");
-});
-user.on("save", () => {
-  console.log("save was trigged");
-});
-
-user.trigger("save");
+user.save();
