177b46c7a49a94546557f1e92dcc2a89a5fc1496
David Landes <davidkailandes@gmail.com>
2022-09-22 15:27:56 -0400

    added some code

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { User } from "./modals/User";
 
 const user = new User({ name: " New Record ", age: 0 });
 
-user.events.on("change", () => {
-  console.log("Change !");
+user.on("Change", () => {
+  console.log("user was changed");
 });
-
-user.events.trigger("change");
